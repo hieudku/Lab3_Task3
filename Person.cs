7ebfd2a0b37ad1a77841279367f0231efe128f86
hieu.cu_it.weltec <hieu.cu_it.weltec@T607-10>
2024-03-14 13:47:26 +1300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Task3
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        private DateTime dateOfBirth;
+
+        public string FirstName { 
+            get => firstName; 
+            set => firstName = value; 
+        }
+        public DateTime DOB
+        {
+            get
+            {
+                return dateOfBirth;
+            }
+        }
+
+        public string LastName { get => lastName;}
+        public string Email { get => emailAddress; }
+
+        public Person(string fName, string lName, string email, DateTime dOB)
+        {
+            firstName = fName;
+            lastName = lName;
+            emailAddress = email;
+            dateOfBirth = dOB;
+        }
+        public Person (string fName, string lName, string email)
+        {
+            firstName = fName;
+            lastName = lName;
+            emailAddress = email;
+        }
+        public Person(string fName, string lName, DateTime dOB)
+        {
+            firstName = fName;
+            lastName = lName;
+            dateOfBirth = dOB;
+        }
+        public void printFields()
+        {
+            Console.WriteLine($"Your detail: {firstName} {lastName} {emailAddress} {dateOfBirth}");
+        }
+        public void chineseZodiac(int year)
+        {
+            // Calculate the Chinese zodiac
+            switch ((year - 4) % 12)
+            {
+                case 0:
+                    Console.WriteLine("Your Chinese zodiac is Rat");
+                    break;
+                case 1:
+                    Console.WriteLine("Your Chinese zodiac is Ox");
+                    break;
+                case 2:
+                    Console.WriteLine("Your Chinese zodiac is Tiger");
+                    break;
+                case 3:
+                    Console.WriteLine("Your Chinese zodiac is Rabbit");
+                    break;
+                case 4:
+                    Console.WriteLine("Your Chinese zodiac is Dragon");
+                    break;
+                case 5:
+                    Console.WriteLine("Your Chinese zodiac is Snake");
+                    break;
+                case 6:
+                    Console.WriteLine("Your Chinese zodiac is Horse");
+                    break;
+                case 7:
+                    Console.WriteLine("Your Chinese zodiac is Goat");
+                    break;
+                case 8:
+                    Console.WriteLine("Your Chinese zodiac is Monkey");
+                    break;
+                case 9:
+                    Console.WriteLine("Your Chinese zodiac is Rooster");
+                    break;
+                case 10:
+                    Console.WriteLine("Your Chinese zodiac is Dog");
+                    break;
+                case 11:
+                    Console.WriteLine("Your Chinese zodiac is Pig");
+                    break;
+            }
+        }
+        public void westernZodiac(int month, int day)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            int month;
+            int day;
+
+            DateTime zeroTime = new DateTime(1, 1, 1);
+
+            Console.WriteLine(DateTime.Today);
+            Console.WriteLine("Enter your date of birth: ");
+            
+
+            Console.WriteLine("Enter year: ");
+            year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter month: ");
+            month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter day: ");
+            day = int.Parse(Console.ReadLine());
+
+
+
+            DateTime date1 = new DateTime(year, month, day);
+
+            Person person1 = new Person("John", "Doe", "john.doe@gmail.com", date1);
+            person1.printFields();
+            person1.chineseZodiac(year);
+            // Check person's age
+            TimeSpan span = DateTime.Now - date1;
+
+            int years = (zeroTime + span).Year - 1;
+            Console.WriteLine(years);
+
+                if (years >= 18)
+                {
+                    Console.WriteLine("You are an adult!");
+                }
+                else if (years < 18 && years > 0)
+                {
+                    Console.WriteLine("Sorry you are not an adult yet!");
+                }
+
+
+            // Calculate birthday
+            if (DateTime.Today.Month == month && DateTime.Today.Day == day)
+            {
+                Console.WriteLine("Today is your birthday! Happy birthday!");
+            }
+
+
+
+            // Calculate the Sun sign
+
+        }
+    }
+}
